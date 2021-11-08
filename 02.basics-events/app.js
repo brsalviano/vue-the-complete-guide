a6d6d69a645adeef06e2aad12e84f21cea44df70
Bruno Salviano da Silva Teixeira <bruno.salviano@gmail.com>
2021-11-08 07:51:50 -0300

    23 - Usando objeto Event nativo

--- 02.basics-events/app.js
+++ 02.basics-events/app.js
@@ -2,9 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    //Podemos receber o objeto Event nativo do javascript...
+    setName(event) {
+      //e utiliza-lo
+      this.name = event.target.value;
+    },
     add(num) {
       this.counter = this.counter + num;
     },
